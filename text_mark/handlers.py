c87eb72863fba3be6bff05bbad34a018d84b429b
wyqbupt <wyqcsnet@yeah.net>
2015-05-17 21:38:04 +0800

    create Handler base class

---
+++ text_mark/handlers.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+class Handler:
+    def callback(self,prefix,name,*args):
+	method = getattr(self,prefix+name,None)
+	if callable(method):
+	    return method(*args)
+    def start(self,name):
+	self.callback('start_',name)
+    def end(self,name):
+	self.callback('end_',name)
+    def sub(self,name):
+	def substitution(match):
+	    result = self.callback('sub_',name,match)
+	    if result is None:
+		match.group(0)
+	    return result
+	return substitution
+
+
