016f14fe81cdf7f4ab7ddfdd206170510f73b874
wyqbupt <wyqcsnet@yeah.net>
2015-05-29 23:20:41 +0800

    remove useless file

--- text_mark/handlers.py
+++ text_mark/handlers.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python
 class Handler:
     def callback(self,prefix,name,*args):
-	method = getattr(self,prefix+name,None)
-	if callable(method):
-	    return method(*args)
+        method = getattr(self,prefix+name,None)
+        if callable(method):
+            return method(*args)
     def start(self,name):
-	self.callback('start_',name)
+        self.callback('start_',name)
     def end(self,name):
-	self.callback('end_',name)
+        self.callback('end_',name)
     def sub(self,name):
-	def substitution(match):
-	    result = self.callback('sub_',name,match)
-	    if result is None:
-		match.group(0)
-	    return result
-	return substitution
+        def substitution(match):
+            result = self.callback('sub_',name,match)
+            if result is None:
+                match.group(0)
+            return result
+        return substitution
 
 class HTMLRenderer(Handler):
 
