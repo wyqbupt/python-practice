9a8cf0e034de927dccc8f011212e007cc8c79a67
wyqbupt <wyqcsnet@yeah.net>
2015-06-10 21:37:41 +0800

    catch all h1 elements

--- XML_web/web_parse.py
+++ XML_web/web_parse.py
@@ -3,8 +3,31 @@ from xml.sax.handler import ContentHandler
 from xml.sax import parse
 
 class Web_Handler(ContentHandler):
-    def startElement(self,name,attrs):
-        print name,attrs.keys()
+    in_headline = False
+    def __init__(self,headlines):
+        ContentHandler.__init__(self)
+        self.headlines = headlines
+        self.data = []
 
-parse('website.xml',Web_Handler())
+    def startElement(self,name,attrs):
+        if name== 'h1':
+	   self.in_headline = True
+
+    def endElement(self,name):
+	if name == 'h1':
+	    text = ''.join(self.data)
+	    self.data = []
+	    self.headlines.append(text)
+	    self.in_headline = False
+
+    def characters(self,string):
+	if self.in_headline:
+	    self.data.append(string)
+
+headlines = []
+
+parse('website.xml',Web_Handler(headlines))
+print 'The following <h1> elements were found:'
+for h in headlines:
+    print h
 
