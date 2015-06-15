78b0fe7134d099e89c038fe6bdabc88f045e0533
wyqbupt <wyqcsnet@yeah.net>
2015-06-15 17:07:19 +0800

    initial web creater

--- XML_web/web_parse.py
+++ XML_web/web_parse.py
@@ -2,32 +2,38 @@
 from xml.sax.handler import ContentHandler
 from xml.sax import parse
 
-class Web_Handler(ContentHandler):
-    in_headline = False
-    def __init__(self,headlines):
+class Web_Maker(ContentHandler):
+    pass_through = False
+    def __init__(self):
         ContentHandler.__init__(self)
-        self.headlines = headlines
         self.data = []
 
     def startElement(self,name,attrs):
-        if name== 'h1':
-	   self.in_headline = True
+        if name== 'page':
+	   self.pass_through = True
+	   self.out = open(attrs['name']+'.html','w')
+	   self.out.write('<html><head>\n')
+	   self.out.write('<title>%s</title\n>'% attrs['title'])
+	   self.out.write('</head><body>')
+	elif self.pass_through:
+	   self.out.write('<'+name)
+	   for key,val in attrs.items():
+               self.out.write(' %s="%s"' % (key,val))
+	   self.out.write('>')
 
     def endElement(self,name):
-	if name == 'h1':
-	    text = ''.join(self.data)
-	    self.data = []
-	    self.headlines.append(text)
-	    self.in_headline = False
+	if name == 'page':
+	    self.pass_through = False
+	    self.out.write('\n</body></html>\n')
+	    self.out.close()
+	elif self.pass_through:
+	    self.out.write('</%s>' % name)
 
     def characters(self,string):
-	if self.in_headline:
-	    self.data.append(string)
+	if self.pass_through:
+	    self.out.write(string)
+    
 
-headlines = []
+parse('website.xml',Web_Maker())
 
-parse('website.xml',Web_Handler(headlines))
-print 'The following <h1> elements were found:'
-for h in headlines:
-    print h
 
