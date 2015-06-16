76c14b17a8466bd620053226f4d45898d91ac53a
wyqbupt <wyqcsnet@yeah.net>
2015-06-16 15:38:04 +0800

    add server clean action after interrupt and add a simple client

--- chater/chat_server.py
+++ chater/chat_server.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python
 from asyncore import dispatcher
 import socket,asyncore
-
+PORT = 5005
 class ChatServer(dispatcher):
+    def __init__(self,port):
+        dispatcher.__init__(self)
+	self.create_socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.set_reuse_addr()
+	self.bind(('',port))
+	self.listen(5)
+
     def handle_accept(self):
         conn,addr = self.accept()
 	print 'Connection attempt from',addr[0]
 
-s = ChatServer()
-s.create_socket(socket.AF_INET,socket.SOCK_STREAM)
-s.bind(('',5005))
-s.listen(5)
-asyncore.loop()
+s = ChatServer(PORT)
+try:
+    asyncore.loop()
+except KeyboardInterrupt:
+    pass
---
+++ chater/client.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':  
+    import socket  
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+    sock.connect(('localhost', 5005))  
+    import time  
+    time.sleep(2)  
+    sock.send('1')  
+    print sock.recv(1024)  
+    sock.close()  
+
